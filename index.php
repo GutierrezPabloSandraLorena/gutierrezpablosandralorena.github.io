f70f77e7142657df4fbaa94afd5170f41cb7aad1
GutierrezPabloSandraLorena <saandygutierrez@gmail.com>
2018-05-06 21:37:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -51,11 +51,24 @@
 		
 		
     <div class="container col s6 l12" id="map"></div>
-		
-		<div class="container col s12 l12" >
+		<div class="input-field col s3 m12 l12">
+		<div class="container col s3 m6 l3" >
+			<div id="izq">
 			<div class="row" id="tweet">
 		</div>
 		</div>
+			</div>
+		</div> 
+			
+			<!--<div class="input-field col s3 m12 l12">
+		 <div class="container col s12 m6 l3" >
+		  <div id="izq">
+		  <div class="row" id="resultados">
+				 
+			</div>
+			
+		</div>
+		</div> -->
 		<div id="tweet1">
 		</div>
 	
